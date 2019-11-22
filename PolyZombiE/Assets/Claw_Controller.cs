b2986d956f520ab64b4f490d198282b64bd97930
Ruiming Chen <ruimingchencareer@gmail.com>
2019-11-22 17:53:53 -0600

    [Master] Minor followup adjustment

--- PolyZombiE/Assets/Claw_Controller.cs
+++ PolyZombiE/Assets/Claw_Controller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Claw_Controller : MonoBehaviour {
+    [SerializeField] float damage = 5.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +14,19 @@ public class Claw_Controller : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void AttackRay(Vector2 from, Vector2 to, LayerMask targetFilter)
+    {
+        Vector2 dir = to - from;
+        float dist = dir.magnitude;
+        RaycastHit2D hit = Physics2D.Raycast(from, dir, dist, targetFilter);
+
+    }
+
+    public void AttackBox(Vector2 from, Vector2 to, float size, LayerMask targetFilter)
+    {
+        Vector2 dir = to - from;
+        float dist = dir.magnitude;
+        //RaycastHit2D hit = Physics2D.BoxCast(from, dir, dist, targetFilter);
+    }
 }
